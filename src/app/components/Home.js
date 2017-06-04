b9a1399ae8473640f2301bee0d37adf4f3a65a4f
phaneendra rao <phaneendra rao>
2017-06-04 15:11:42 +0530

    third commit

---
+++ src/app/components/Home.js
@@ -0,0 +1,12 @@
+import React from  "react";
+
+export class Home extends React.Component{
+  render()
+  {
+    return (
+        <div className="container">
+          <p>In a new Component</p>
+        </div>
+    );
+  }
+}
